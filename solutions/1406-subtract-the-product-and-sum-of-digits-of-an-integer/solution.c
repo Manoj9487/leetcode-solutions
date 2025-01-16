afb08392fe70f341ec65ce26498372b2f4fc6551
Venkat Manoj <manojkudupudi143@gmail.com>
2025-01-16 09:27:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (100.00%)

---
+++ solutions/1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.c
@@ -0,0 +1,10 @@
+int subtractProductAndSum(int n) {
+    int rem, sum = 0, product = 1;
+    while(n > 0) {
+        rem = n % 10;
+        sum += rem;
+        product *= rem;
+        n /= 10;
+    }
+    return product - sum;
+}
