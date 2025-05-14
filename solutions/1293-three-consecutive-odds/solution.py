09299a2b2520a7de7d9f1eb993cf47e323fc61b7
Venkat Manoj <manojkudupudi143@gmail.com>
2025-05-14 08:19:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ solutions/1293-three-consecutive-odds/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd = 0
+        for i in arr :
+            if i % 2 == 1 :
+                odd += 1
+                if odd == 3 :
+                    return True
+                    break
+            else :
+                odd = 0
+        return False
