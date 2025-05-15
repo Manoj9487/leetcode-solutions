b12a536f72c06b53b4068d2340ff5fe95ecca0cc
Venkat Manoj <manojkudupudi143@gmail.com>
2025-05-15 14:26:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

---
+++ solutions/3778-transform-array-by-parity/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        l = []
+        for i in nums :
+            if i % 2 == 0 :
+                l.append(0)
+            else :
+                l.append(1)
+        return sorted(l)
+
