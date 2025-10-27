3c79610c86d1da5deb79762e501678fc6b1c3b92
Venkat Manoj <manojkudupudi143@gmail.com>
2025-10-27 17:41:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (100.00%)

---
+++ solutions/0203-remove-linked-list-elements/solution.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+
+
+    if (head == NULL) {
+        return head;
+    }
+    while(head!= NULL && head->val==val){
+            head = head->next;
+    }
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while(curr != NULL) {
+        if (curr->val == val) {
+            prev->next = curr->next;
+            curr = curr->next;
+
+        }
+        else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    return head;
+}
