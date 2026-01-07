8e4e609a5010f9fc306a99a79d731a0480acd8bb
Venkat Manoj <manojkudupudi143@gmail.com>
2026-01-07 17:07:51 +0000

    Sync LeetCode submission Runtime - 77 ms (31.78%), Memory - 29.9 MB (24.54%)

---
+++ solutions/1683-maximum-number-of-coins-you-can-get/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        total = 0
+        piles.sort(reverse = True)
+        for i in range(1, len(piles) - len(piles) // 3, 2) :
+            total += piles[i]
+        return total
+        
