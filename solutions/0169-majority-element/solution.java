b0bdb2ab7a4aa4dd7dc741cdc0d2229ea68e4a0f
Venkat Manoj <manojkudupudi143@gmail.com>
2026-01-30 17:31:27 +0000

    Sync LeetCode submission Runtime - 3 ms (50.00%), Memory - 56.1 MB (6.34%)

--- solutions/0169-majority-element/solution.java
+++ solutions/0169-majority-element/solution.java
@@ -1,16 +1,29 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int ele = 0;
+        int cnt = 0;
+        for (int i : nums) {
+            if (cnt == 0) {
+                cnt += 1;
+                ele = i;
+            }
+            else if (i == ele) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
 
-        Map <Integer, Integer> hashmap = new HashMap<>();
-
-        for (int num : nums) {
-            hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
-
-            if (hashmap.get(num) > Math.floor(nums.length / 2)) {
-                return num;
+        int cnt1 = 0;
+        for(int i : nums) {
+            if (i == ele) {
+                cnt1 += 1;
+            }
+            if (cnt1 > Math.floor(nums.length / 2)) {
+                return ele;
             }
         }
         return -1;
-
     }
 }
