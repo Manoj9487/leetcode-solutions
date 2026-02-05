0ee7e9ca6b2a8186e9f28f15b21fab72e351738f
Venkat Manoj <manojkudupudi143@gmail.com>
2026-02-05 17:08:13 +0000

    Sync LeetCode submission Runtime - 1 ms (98.36%), Memory - 46.7 MB (53.62%)

---
+++ solutions/3651-transformed-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                arr[i] = nums[i];
+            }
+            else {
+                arr[i] = nums[(i + nums[i] % n + n) % n];
+            }
+        }
+        return arr;
+    }
+}
