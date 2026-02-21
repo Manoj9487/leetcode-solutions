45a54dfdc77f5fb65ae994e0fa0eae7da404c807
Venkat Manoj <manojkudupudi143@gmail.com>
2026-02-21 17:35:45 +0000

    Sync LeetCode submission Runtime - 2 ms (99.33%), Memory - 72.2 MB (92.80%)

---
+++ solutions/0890-lemonade-change/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int change5 = 0, change10 = 0;
+
+        for(int x : bills) {
+            if (x == 5) {
+                change5 += 1;
+            }
+            else if (x == 10) {
+                if (change5 > 0) {
+                    change5 -= 1;
+                    change10 += 1;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if (change5 > 0 && change10 > 0) {
+                    change5 -= 1;
+                    change10 -= 1;
+                }
+                else if (change5 > 2) {
+                    change5 -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
