899b8b4cf83aa4706a33254162634e78e51f5b7e
Venkat Manoj <manojkudupudi143@gmail.com>
2026-03-01 04:16:08 +0000

    Sync LeetCode submission Runtime - 1 ms (99.73%), Memory - 44.3 MB (67.70%)

---
+++ solutions/4229-trim-trailing-vowels/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String trimTrailingVowels(String s) {
+        int i = s.length() - 1;
+
+        while(i >= 0 && isVowel(s.charAt(i))) {
+            i--;
+        }
+
+        if (i < 0) return "";
+
+        return s.substring(0, i + 1);
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' | c == 'e' | c == 'i' | c == 'o' | c == 'u';
+    }
+}
