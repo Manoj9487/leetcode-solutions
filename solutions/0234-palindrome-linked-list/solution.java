4896261de863f0cc97a56a96f793c39811a9d32c
Venkat Manoj <manojkudupudi143@gmail.com>
2026-05-18 07:45:55 +0000

    Sync LeetCode submission Runtime - 13 ms (11.96%), Memory - 98.9 MB (25.06%)

--- solutions/0234-palindrome-linked-list/solution.java
+++ solutions/0234-palindrome-linked-list/solution.java
@@ -9,25 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head) {
+    private ListNode reverseLinkedList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode newHead = reverse(head.next);
 
+        ListNode newHead = reverseLinkedList(head.next);
+        
         ListNode front = head.next;
-
         front.next = head;
         head.next = null;
 
         return newHead;
-
     }
-    public boolean isPalindrome(ListNode head) {
 
+    public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
             return true;
         }
+
         ListNode slow = head;
         ListNode fast = head;
 
@@ -36,21 +36,23 @@ class Solution {
             fast = fast.next.next;
         }
 
-        ListNode newHead = reverse(slow.next);
+        ListNode newHead = reverseLinkedList(slow.next);
 
         ListNode first = head;
         ListNode second = newHead;
 
         while(second != null) {
-            if (first.val != second.val) {
-                reverse(newHead);
+            if(first.val != second.val) {
+                reverseLinkedList(newHead);
                 return false;
             }
+
             first = first.next;
             second = second.next;
         }
-        reverse(newHead);
-        return true;
 
+        reverseLinkedList(newHead);
+        return true;
+        
     }
 }
