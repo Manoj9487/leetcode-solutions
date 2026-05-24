0acb0a914e959a3006d25b44bdb1f2af266f9ce6
Venkat Manoj <manojkudupudi143@gmail.com>
2026-05-24 15:48:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (31.04%)

---
+++ solutions/0138-copy-list-with-random-pointer/solution.java
@@ -0,0 +1,71 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    private void insertCopyInBetween(Node head) {
+        Node temp = head;
+
+        while(temp != null) {
+            Node nextElement = temp.next;
+
+            Node copy = new Node(temp.val);
+
+            copy.next = nextElement;
+            temp.next = copy;
+
+            temp = nextElement;
+        }
+    }
+
+    private void connectRandomPointers(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            Node copyNode = temp.next;
+            
+            if (temp.random != null) {
+                copyNode.random = temp.random.next;
+            }
+            else {
+                copyNode.random = null;
+            }
+
+            temp = temp.next.next;
+        }
+    }
+
+    private Node getDeepCopyList(Node head) {
+        Node temp = head;
+        
+        Node dummy = new Node(-1);
+        Node res = dummy;
+
+        while(temp != null) {
+            res.next = temp.next;
+            res = res.next;
+
+            temp.next = temp.next.next;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
+    }
+}
