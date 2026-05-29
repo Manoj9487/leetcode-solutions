2388cfdc0844acf9a98d0424f9c38ed9dfd30896
Venkat Manoj <manojkudupudi143@gmail.com>
2026-05-29 08:05:57 +0000

    Sync LeetCode submission Runtime - 1 ms (99.78%), Memory - 47.3 MB (8.27%)

---
+++ solutions/2032-largest-odd-number-in-string/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int ind = -1;
+        int i;
+        for(i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 == 1) {
+                ind = i;
+                break;
+            }
+        }
+        
+        if (ind == -1) return "";
+
+        return num.substring(0, ind + 1);
+
+    }
+}
