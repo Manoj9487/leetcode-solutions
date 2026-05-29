0ae9ae01736f312c141693c008d2e78b72b499b0
Venkat Manoj <manojkudupudi143@gmail.com>
2026-05-29 10:48:50 +0000

    Sync LeetCode submission Runtime - 5 ms (61.93%), Memory - 44.1 MB (94.72%)

---
+++ solutions/0242-valid-anagram/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int [] count = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a'] += 1;
+            count[t.charAt(i) - 'a'] -= 1;
+        }
+
+        for(int cnt : count) {
+            if (cnt != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
