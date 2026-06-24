0be3f74600c99a17dbb5b77069effe719855312c
Venkat Manoj <manojkudupudi143@gmail.com>
2026-06-24 14:23:47 +0000

    Sync LeetCode submission Runtime - 2 ms (99.71%), Memory - 45.2 MB (42.17%)

---
+++ solutions/0090-subsets-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    private void findSubsets(int ind, int[] arr, List<List<Integer>> ans, List<Integer> ds) {
+        ans.add(new ArrayList<>(ds));
+        for(int i = ind; i < arr.length; i++) {
+            if (i != ind && arr[i] == arr[i - 1]) continue;
+            ds.add(arr[i]);
+            findSubsets(i + 1, arr, ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        findSubsets(0, nums, ans, new ArrayList<>());
+        return ans;
+    }
+}
