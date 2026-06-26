c05535b0faa16d3d2f567e7e94c40a51e2d4fa4d
Venkat Manoj <manojkudupudi143@gmail.com>
2026-06-26 18:27:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.8 MB (56.28%)

---
+++ solutions/0042-trapping-rain-water/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0, r = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int total = 0;
+
+        while(l < r) {
+            if (height[l] <= height[r]) {
+                if (height[l] < leftMax) {
+                    total += leftMax - height[l];
+                }
+                else {
+                    leftMax = height[l];
+                }
+                l++;
+            }
+            else {
+                if (height[r] < rightMax) {
+                    total += rightMax - height[r];
+                }
+                else {
+                    rightMax = height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+}
