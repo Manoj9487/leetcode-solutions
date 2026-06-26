5fc8e02c28072f32254fedd611b775b2dbe6b35a
Venkat Manoj <manojkudupudi143@gmail.com>
2026-06-26 15:40:22 +0000

    Sync LeetCode submission Runtime - 2 ms (99.42%), Memory - 45.5 MB (22.31%)

--- solutions/0496-next-greater-element-i/solution.java
+++ solutions/0496-next-greater-element-i/solution.java
@@ -3,18 +3,16 @@ class Solution {
         int[] nextGreater = new int[10001];
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]) {
                 stack.pop();
             }
             nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(nums2[i]);
         }
-
-        for (int i = 0; i < nums1.length; i++) {
+        for(int i = 0; i < nums1.length; i++) {
             nums1[i] = nextGreater[nums1[i]];
         }
-
         return nums1;
     }
 }
