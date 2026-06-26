b2bb401b74b3908304b69244a76462d7fafc4fca
Venkat Manoj <manojkudupudi143@gmail.com>
2026-06-26 05:48:28 +0000

    Sync LeetCode submission Runtime - 1 ms (88.33%), Memory - 44.6 MB (7.38%)

--- solutions/0078-subsets/solution.java
+++ solutions/0078-subsets/solution.java
@@ -1,18 +1,16 @@
 class Solution {
-    private void findCombinations(int ind, int[] arr, List<List<Integer>> ans, List<Integer> ds) {
-        if (ind == arr.length) {
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
-        ds.add(arr[ind]);
-        findCombinations(ind + 1, arr, ans, ds);
-        ds.remove(ds.size() - 1);
-        findCombinations(ind + 1, arr, ans, ds);
-
-    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, nums, ans, new ArrayList<>());
+        int n = nums.length;
+        for(int i = 0; i < (1 << n); i++) {
+            List<Integer> ds = new ArrayList<>();
+            for(int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    ds.add(nums[j]);
+                }
+            }
+            ans.add(new ArrayList<>(ds));
+        }
         return ans;
     }
 }
