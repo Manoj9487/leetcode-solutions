2372952b40742562e4de70aa6f18fc2b990018cb
Venkat Manoj <manojkudupudi143@gmail.com>
2026-06-29 05:53:21 +0000

    Sync LeetCode submission Runtime - 7 ms (36.70%), Memory - 46.8 MB (14.03%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.java
+++ solutions/0003-longest-substring-without-repeating-characters/solution.java
@@ -3,21 +3,23 @@ class Solution {
         int n = s.length();
         int maxLength = 0;
         Set<Character> charSet = new HashSet<>();
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (!charSet.contains(s.charAt(right))) {
-                charSet.add(s.charAt(right));
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                while (charSet.contains(s.charAt(right))) {
-                    charSet.remove(s.charAt(left));
-                    left++;
-                }
-                charSet.add(s.charAt(right));
+        int l = 0, r = 0;
+
+        while(r < n) {
+            if (!charSet.contains(s.charAt(r))) {
+                charSet.add(s.charAt(r));
+                maxLength = Math.max(maxLength, r - l + 1);
             }
+            else {
+                while(charSet.contains(s.charAt(r))) {
+                    charSet.remove(s.charAt(l));
+                    l++;
+                }
+                charSet.add(s.charAt(r));
+            }
+            r++;
         }
-        
         return maxLength;
     }
 }
+
