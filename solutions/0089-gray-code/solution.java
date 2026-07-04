2f7aa4a7f06cd480adb4098e5072ab3c4f3f3c93
Venkat Manoj <manojkudupudi143@gmail.com>
2026-07-04 06:42:45 +0000

    Sync LeetCode submission Runtime - 4 ms (99.46%), Memory - 53.7 MB (57.14%)

--- solutions/0089-gray-code/solution.java
+++ solutions/0089-gray-code/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> l1=new ArrayList<>();
-        int e=((int)Math.pow(2,n))-1;
-        for(int i=0;i<=e;i++){
-            l1.add(i^(i>>1));
+        int e = (int) Math.pow(2, n) - 1;
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i <= e; i++) {
+            ans.add(i ^ (i >> 1));
         }
-        return l1;
+        return ans;
     }
 }
