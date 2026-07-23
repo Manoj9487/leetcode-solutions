2879bb53fc249e3769c4e7359db298e3fce6fa7d
Venkat Manoj <manojkudupudi143@gmail.com>
2026-07-23 15:30:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 125.4 MB (46.36%)

---
+++ solutions/3824-number-of-unique-xor-triplets-i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int len = nums.length;
+        if (len < 3) return len;
+
+        int ans = 1;
+
+        while(ans <= len) {
+            ans <<= 1;
+        }
+        
+        return ans;
+    }
+}
