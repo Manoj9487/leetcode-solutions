c8716967a1f8195ac2c025134b4f82a99612acad
Venkat Manoj <manojkudupudi143@gmail.com>
2026-07-27 17:32:39 +0000

    Sync LeetCode submission Runtime - 5 ms (42.30%), Memory - 45.2 MB (12.63%)

--- solutions/1574-maximum-product-of-two-elements-in-an-array/solution.java
+++ solutions/1574-maximum-product-of-two-elements-in-an-array/solution.java
@@ -1,14 +1,7 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length - 1; j++) {
-                if (nums[i] < nums[j]) {
-                        int t = nums[j];
-                        nums[j] = nums[i];
-                        nums[i] = t;
-                }
-            }
-        }
-        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+        int n = nums.length - 1;
+        Arrays.sort(nums);
+        return (nums[n] - 1) * (nums[n - 1] - 1);
     }
 }
